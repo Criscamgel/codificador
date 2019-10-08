3502d0dfc292688ffc11ce9766d5afcc64b16fca
Camilo Guio <camiloguio@gmail.com>
2019-10-08 18:35:27 -0500

    Codificando

--- app.js
+++ app.js
@@ -20,9 +20,10 @@
     ]; */
 let sumTemp; /* Suma temporal dos ultimas posiciones */
 let temp = [];
-let tempX = new Array(1); /* arreglo temporal Últimas dos posiciones */
+let tempX = new Array(0); /* arreglo temporal Últimas dos posiciones */
 let tempJson = [];/* Nuevo Arreglo despues de eliminar el remanente */
-let matrix = new Array(1);
+let matrix = new Array(0);/* Matrix que alojará todas las operaciones*/
+let binario = new Array(0); /* Matriz que alojará el codigo binario */
 
 /* Igualación de la Matrix */
 matrix[0] = json;
@@ -32,25 +33,64 @@ console.log("----------------------------");
 
         for (let index = 0; index < matrix.length; index++) {
             if(matrix[index].length !== 2){
-                
+            sumTemp = '';    
             /* Dos posiciones temporales */
             temp = matrix[index].slice(-1 * base);
             /* Añadiendo a la matrix de codificación */
             tempX.push(temp);
             /* Suma del remanente, convierte a decimal de dos posiciones */
             sumTemp = parseFloat(temp.reduce(function(temp, b) { return temp + b; }, 0).toFixed(2));
+            console.log(sumTemp);          
+            /* Añadiendo la suma a la matriz de codificación */
+            tempX[index].push(sumTemp + 0.001);
             /* Quitando dos posiciones del arreglo  general y dejandolas en uno nuevo */
             tempJson = matrix[index].slice(0, base * -1);
-            /* sumTemp = parseFloat((sumTemp).toFixed(2)); */
             /* Añadiendo la suma del remanente al arreglo temporal */
-            tempJson.push(sumTemp);
-            
-            
+            sumTemp += 0.001;
+            tempJson.push(sumTemp);            
             /* Organizando el arreglo */        
             tempJson.sort().reverse();
-            matrix.push(tempJson); 
+            /* Haciendo la incorporación a la matriz final */
+            matrix.push(tempJson);                         
             }
         }
+        /* Reorganizando el arreglo de verificación */
+        tempX.reverse();
+
+        /* Codificando */
+        for (let index = 0; index < matrix.length; index++) {
+          if(index != 0){
+          console.log(matrix[matrix.length-index], "Reversa Matrix");
+
+          for (let bin = 0; bin <= 1; bin++) {
+            if(binario.length > 0){
+              console.log(binario[index], "binario[index]");
+              binario[index].push([bin]);                                       
+            }else{
+              binario.push(bin); 
+            }
+          }
+
+          }else{
+          /* Poniedo Ceros y unos en la primera posición - Excepción 1 */
+            /* array.forEach(element => { */
+              for (let bin = 0; bin <= 1; bin++) {
+                  if(binario.length > 0){
+                    console.log(binario[index], "binario[index]");
+                    binario[index].push(bin);                                       
+                  }else{
+                    binario.push([bin]); 
+                  }
+                }
+            /* }); */
+            
+          }
+          
+          if(tempX[index]){
+            console.log(tempX[index][tempX[index].length-1], "tempX[Clave]");       
+          }          
+        }
+        
         
 function generaTabla(){
         console.log(matrix);
